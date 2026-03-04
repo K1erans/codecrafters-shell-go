858299753123a6a98e5bcfab6a47e8343ae1010c
Kieran <krkearney11@outlook.com>
2026-03-04 22:27:04 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,34 +4,42 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	reader := bufio.NewReader(os.Stdin)
+	commands := []string{"echo", "type"}
 	for {
 		fmt.Print("$ ")
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
-		if input == "exit" {
-			return
-		}
-
-		Command(strings.Fields(input))
+		Command(strings.Fields(input), commands)
 	}
 }
 
-func Command(command []string) {
-	switch command[0] {
+func Command(args []string, commands []string) {
+	switch args[0] {
 	case "echo":
 		var output string
-		for _, arg := range command[1:] {
+		for _, arg := range args[1:] {
 			output += arg + " "
 		}
 		fmt.Println(output)
 		return
+	case "type":
+		if slices.Contains(commands, args[1]) {
+			fmt.Println(args[1] + " is a shell builtin")
+		} else {
+			fmt.Println(args[1] + ": not found")
+		}
+		return
+	case "exit":
+		os.Exit(1)
+		return
 	default:
-		fmt.Println(command[0] + ": command not found")
+		fmt.Println(args[0] + ": command not found")
 	}
 }
