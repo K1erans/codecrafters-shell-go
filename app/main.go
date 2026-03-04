0b2b7325c5e6c9cf0a0fa1512639c1eb135fdfa1
Kieran <krkearney11@outlook.com>
2026-03-04 22:18:43 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	var input string
+	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
-		fmt.Scanln(&input)
-		switch input {
-		case "exit":
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if input == "exit" {
 			return
-		default:
-			fmt.Println(input + ": command not found")
 		}
+
+		Command(strings.Fields(input))
+	}
+}
+
+func Command(command []string) {
+	switch command[0] {
+	case "echo":
+		var output string
+		for _, arg := range command[1:] {
+			output += arg + " "
+		}
+		fmt.Println(output)
+		return
+	default:
+		fmt.Println(command[0] + ": command not found")
 	}
 }
